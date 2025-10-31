a19b1f5587d5cf2d9de576e3a566d8bb05afb3d3
Aiden Fine <aiden03fine@gmail.com>
2025-10-31 11:16:51 -0400

    remove old outdated sliding window code

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"time"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
 
 	"github.com/aidenfine/checkpoint"
 )
 
 func main() {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hello world")
-	})
-	rlMw := checkpoint.LimitByIp(15, 30*time.Second)
-	handler := rlMw(mux)
+	serviceUrl := os.Getenv("SERVICE_URL")
 
-	http.ListenAndServe(":8080", handler)
+	if serviceUrl == "" {
+		panic("missing SERVICE_URL in env!")
+	}
+	backendURL, _ := url.Parse(serviceUrl)
+	proxy := httputil.NewSingleHostReverseProxy(backendURL)
+
+	limiter := checkpoint.NewTokenBucket(1, 100, 5)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ip := getIP(r)
+		allowed, remaining := limiter.Allow(ip)
+		if !allowed {
+			w.WriteHeader(http.StatusTooManyRequests)
+			fmt.Fprintf(w, "Rate limit exceeded! Try again later.\n")
+			return
+		}
+
+		r.Header.Set("X-RateLimit-Remaining", fmt.Sprintf("%d", remaining))
+		fmt.Printf("[%s] allowed: %d tokens left\n", ip, remaining)
+
+		proxy.ServeHTTP(w, r)
+	})
+
+	fmt.Println("Reverse proxy :8080 → forwarding to :8000")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getIP(r *http.Request) string {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip != "" {
+		parts := strings.Split(ip, ",")
+		return strings.TrimSpace(parts[0])
+	}
+
+	ip = strings.Split(r.RemoteAddr, ":")[0]
+	return ip
 }
