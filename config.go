798e54a34bd62e0ca3d516f83e7db95e4b18ea8b
Aiden Fine <aiden03fine@gmail.com>
2025-10-31 12:57:25 -0400

    add config setup

--- config.go
+++ config.go
@@ -1,1 +1,46 @@
 package checkpoint
+
+import (
+	"os"
+	"sync"
+)
+
+type Config struct {
+	ServiceUrl string
+	Logs       LogConfig
+}
+
+var (
+	cfg  *Config
+	once sync.Once
+)
+
+type LogConfig struct {
+	Level string
+}
+
+func LoadConfigs() {
+	once.Do(func() {
+		cfg = &Config{
+			ServiceUrl: os.Getenv("SERVICE_URL"),
+			Logs: LogConfig{
+				Level: getEnvWithDefault("LOG_LEVEL", "ERROR"),
+			},
+		}
+	})
+}
+func getEnvWithDefault(key, def string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return def
+	}
+	return val
+
+}
+
+func Get() *Config {
+	if cfg == nil {
+		panic("Config not loaded, call config.Load()") // may not need to panic here
+	}
+	return cfg
+}
