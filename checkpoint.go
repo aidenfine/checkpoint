4096ab1f98cfb8821277e945af2d05d530348b04
Aiden Fine <aiden03fine@gmail.com>
2026-02-07 14:54:53 -0500

    remove limit by endpoint

--- checkpoint.go
+++ checkpoint.go
@@ -11,9 +11,6 @@ func Limit(maxTokens, refillRate, tokensPerRefill int, config Config) func(next 
 func LimitByIp(maxTokens, refillRate, tokensPerRefill int, config Config) func(next http.Handler) http.Handler {
 	return Limit(maxTokens, refillRate, tokensPerRefill, config)
 }
-func LimitIpByEndpoint(maxTokens, refillRate, tokensPerRefill int, config Config) func(next http.Handler) http.Handler {
-	return Limit(maxTokens, refillRate, tokensPerRefill, config)
-}
 
 func WithConfig(config Config) func(next http.Handler) http.Handler {
 	return config.LimitMethod(config.MaxTokens, config.RefillRate, config.TokensPerRefill, config)
